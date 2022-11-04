35fbea5d16b0ec422bdd80f3d37588eb0e0f44ba
teodor.ivanov <teddye30@gmail.com>
2022-11-04 21:44:30 +0200

    Navigation made with ViewConponent

---
+++ UmbracoProject1/Components/NavigationViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using UmbracoProject1.Models;
+
+namespace UmbracoProject1.Components
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(IPublishedContent content)
+        {
+            var homePage = content.AncestorOrSelf<HomePage>();
+            var navigationItems = homePage?.MainNavigation ?? Enumerable.Empty<NavigationItem>();
+
+            var model = new NavigationViewModel()
+            {
+                HomeUrl = homePage.Url(),
+                NavigationItems = navigationItems
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ UmbracoProject1/Models/NavigationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace UmbracoProject1.Models
+{
+    public class NavigationViewModel
+    {
+        public string HomeUrl { get; set; }
+        public IEnumerable<NavigationItem> NavigationItems { get; set; }
+    }
+}
